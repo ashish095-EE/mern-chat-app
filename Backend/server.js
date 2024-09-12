841ff01b96f0369ad4b3250130b4d33e81f62166
ashish095-EE <127418703+ashish095-EE@users.noreply.github.com>
2024-09-12 18:27:19 +0530

    env file changes

--- Backend/server.js
+++ Backend/server.js
@@ -2,19 +2,25 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.routes.js';
+import connectToMongoDB from './db/connectToMongoDB.js';
 
 dotenv.config(); //load environment variables from.env file
 
 const app = express();
-const port =  process.env.PORT || 5000;
+const port =  process.env.PORT
 
 app.get('/', (req, res) => {
     //root route localhost PORT env 
-    res.send('Welcome to my API');
+    res.send('welcome to my API');
 });
 
 app.use("/api/auth", authRoutes);
 
-app.listen(port, ()=> console.log(`port active on ${port}`));
+app.listen(port, ()=>{
+    connectToMongoDB();
+    console.log(`port active on ${port}`);
+    
+
+});
   
 
