265b3c1ab77118623c1da1a4fa5dcad0be124449
ashish095-EE <127418703+ashish095-EE@users.noreply.github.com>
2024-10-23 04:40:21 +0530

    lil change

--- Backend/db/connectToMongoDB.js
+++ Backend/db/connectToMongoDB.js
@@ -11,7 +11,7 @@ dotenv.config();
 const connectToMongoDB = async () =>{
     try {
         console.log("MongoDB URI:", process.env.MONGO_DB_URI);
-        await mongoose.connect("mongodb+srv://ashishmohapatraofficial:gamu7I7FPzzgSr3L@cluster0.l9asw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+        await mongoose.connect(process.env.MONGO_DB_URI);
         console.log("MongoDB Connected");
         
     } catch (error) {
