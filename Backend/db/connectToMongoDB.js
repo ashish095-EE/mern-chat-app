f018e963851239df003215af756be6826d902ddd
ashish095-EE <127418703+ashish095-EE@users.noreply.github.com>
2024-10-23 04:29:47 +0530

    ready for deployment

--- Backend/db/connectToMongoDB.js
+++ Backend/db/connectToMongoDB.js
@@ -1,11 +1,17 @@
 import mongoose from "mongoose";
 
+import dotenv from 'dotenv';
+
+// Load environment variables
+dotenv.config();
+
 
 
 
 const connectToMongoDB = async () =>{
     try {
-        await mongoose.connect(process.env.MONGO_DB_URI,);
+        console.log("MongoDB URI:", process.env.MONGO_DB_URI);
+        await mongoose.connect("mongodb+srv://ashishmohapatraofficial:gamu7I7FPzzgSr3L@cluster0.l9asw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
         console.log("MongoDB Connected");
         
     } catch (error) {
--- Backend/server.js
+++ Backend/server.js
@@ -1,4 +1,4 @@
-
+import path from "path";
 import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.routes.js';
@@ -8,12 +8,14 @@ import connectToMongoDB from './db/connectToMongoDB.js';
 import cookieParser from 'cookie-parser';
 import {app,server} from "./socket/Socket.js";
 
+
  
 
 
 
 
 const PORT =  process.env.PORT||5000;
+const __dirname = path.resolve();
 dotenv.config(); //load environment variables from.env file
 
 app.use(express.json());
@@ -28,6 +30,13 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname,"/Frontend/dist")))
+
+app.get("*", (req,res)=>{
+    res.sendFile(path.join(__dirname,"Frontend","dist","index.html"));
+
+})
 server.listen(PORT, ()=>{
     connectToMongoDB();
     console.log(`port active on ${PORT}`);
